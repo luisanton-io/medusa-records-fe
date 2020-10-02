0177797722d782eff4bfb8db428848090275ba3b
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-02 21:19:47 +0200

    Update Home.tsx

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Routes } from '../../routes/Routes'
 
 export default class Home extends React.Component {
     render() {
-        return <div>I am Home.</div>
+        return <div>I am Home. <Link to ={Routes.private.submissions.selectList}>Submissions</Link></div>
     }
 }
