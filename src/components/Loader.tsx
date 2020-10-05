45f9ce4f4cf1af366e739ba9e09a949bf12cbcd0
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-05 12:27:35 +0200

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ import { theme } from '../App'
 export default function Loader() {
     return <div className="position-absolute bg-dark d-flex" style={{top: "0", left: "0", height: "100vh", width: "100vw"}}>
         <div className="m-auto d-flex flex-column align-items-center" >
-            <img className="img-fluid w-50 mb-5" src="/assets/medusa-original.png" />
+            <img className="img-fluid w-50 mb-5" src="/assets/medusa-original.png" alt="..." />
             <CircularProgress style={{ color: theme.palette.primary.dark }} />
         </div>
     </div>
