cbe3ed05e83b346e7bb54d507bf7eef4a271256f
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-07 22:30:11 +0200

    Update Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../styles/Player.scss'
 import { AppBar, Toolbar } from '@material-ui/core'
 import {
@@ -10,10 +10,14 @@ import {
     VolumeOffRounded as VolumeOff,
     VolumeMuteRounded as VolumeMute,
     VolumeDownRounded as VolumeDown,
-    VolumeUpRounded as VolumeUp
+    VolumeUpRounded as VolumeUp,
+    PauseRounded as Pause
 } from '@material-ui/icons'
 
-interface PlayerState {
+import AudioComponent from '../models/AudioComponent'
+import { State } from 'react-flux-component'
+
+interface PlayerState extends State {
     shuffling: boolean,
     cycling: boolean,
     muted: boolean,
@@ -21,7 +25,14 @@ interface PlayerState {
     volume: number
 }
 
-export default class Player extends Component<{}, PlayerState> {
+export default class Player extends AudioComponent<{}, PlayerState> {
+
+    constructor(props: {}) {
+        super(props)
+        this.audioPlayer = React.createRef()
+    }
+
+    private audioPlayer: React.RefObject<HTMLAudioElement>;
 
     state: PlayerState ={
         ...this.state,
@@ -32,26 +43,30 @@ export default class Player extends Component<{}, PlayerState> {
         volume: 100
     }
 
-    didToggleShuffle = () => {
-        this.setState({shuffling: !this.state.shuffling}, () =>
-            console.log(`Now ${this.state.shuffling ? "" : "not"} shuffling.`)
-        )
+    nowPlaying = this.hardBind(AudioComponent.shared.nowPlaying)
+    playlist = this.hardBind(AudioComponent.shared.playlist)
+
+    componentDidUpdate = () => {
+        const player = this.audioPlayer.current!
+        const index = this.nowPlaying.value
+
+        if (this.state.paused && !player.paused) { //
+            player.pause()
+        } else if (player.paused && index !== null) {
+            this.state.paused //if it was initialized and later paused...
+                ? this.setState({ paused: false }) //...now set we are playing
+                : player.play()
+        } 
     }
 
-    didToggleCycle = () => {
-        this.setState({cycling: !this.state.cycling}, () =>
-            console.log(`Now ${this.state.cycling ? "" : "not"} cycling.`)
-        )
-    }
-    
-    didToggleMute = () => {
-        this.setState({muted: !this.state.muted}, () =>
-            console.log(`Now ${this.state.muted ? "" : "un"}muted`)
-        )
-    }
+    didToggleShuffle = () => this.setState({shuffling: !this.state.shuffling})
+    didToggleCycle = () => this.setState({cycling: !this.state.cycling})
+    didToggleMute = () => this.setState({muted: !this.state.muted}) 
 
     tweakVolume = ({currentTarget}: React.FormEvent<HTMLInputElement>) => {
-        this.setState({muted: false, volume: parseInt(currentTarget.value)})
+        this.setState({muted: false, volume: parseInt(currentTarget.value)}, () => 
+            this.audioPlayer.current!.volume = this.state.volume / 100
+        )
         console.log("Volume is: " + currentTarget.value)
     }
 
@@ -59,29 +74,55 @@ export default class Player extends Component<{}, PlayerState> {
         console.log("Did press REWARD button.")
     }
 
+    getRandomIx = () => {
+        return Math.floor(Math.random() * Math.floor(this.playlist.value.length))
+    }
+
     forward = () => {
         console.log("Did press FORWARD button.")
+
+        if (this.nowPlaying.value !== null) {
+            this.nowPlaying.value = this.state.shuffling
+                                        ? this.getRandomIx()
+                                        : this.nowPlaying.value + 1
+        }
     }
 
-    play = () => {
-        console.log("Now playing...")
+    playClicked = () => {
+        this.setState({paused: false})
     }
 
-    pause = () => {
-        console.log("Paused.")
+    pauseClicked = () => {
+        this.setState({paused: true})
     }
 
     render() {
-        const {cycling, muted, paused, shuffling } = this.state
+        const { cycling, muted, shuffling, paused } = this.state
         const volume = muted ? 0 : this.state.volume
+
+        const onAir = this.nowPlaying.value !== null ? this.playlist.value[this.nowPlaying.value] : null
+        const player = this.audioPlayer.current
+
         return <AppBar 
             style={{position: "relative"}}
             >
             <Toolbar className="bg-dark text-white px-0" id="player-footer">
-                <div id="title-display" className="ml-3 my-1 text-white" style={{width: "auto"}}>
-                    <h5 id="track-title">-</h5>
-                    <span id="track-artist">-</span>
-                </div>
+                <audio src={onAir?.audioURL} id='audio-player' ref={this.audioPlayer} />  
+                {
+                    onAir &&
+                    <div id="title-display" className="ml-3 my-1 text-white" style={{width: "auto"}}>
+                        <h5 id="track-title">{onAir.title}</h5>
+                        <span id="track-artist"> 
+                            {
+                                `${onAir.mainArtists.join(', ')}${
+                                    onAir.featurings.length > 0 
+                                        ? ` feat. ${onAir.featurings.join(', ')}` 
+                                        : '' 
+                                }` 
+                            }
+                        </span>
+                    </div>
+                }
                 <div id="player-controls-wrapper" className="d-flex flex-column mx-auto">
                     <div id="player-controls" className="d-flex justify-content-around align-items-center mx-auto">
                         <label className={`form-check-label d-flex align-items-center ${shuffling ? "control-toggled" : ""}`}>
@@ -90,7 +131,10 @@ export default class Player extends Component<{}, PlayerState> {
                             <Shuffle />
                         </label>
                         <SkipPrevious onClick={this.reward} />
-                        <PlayArrow onClick={() => paused ? this.play() : this.pause()} />
+                        {
+                            paused  ? <PlayArrow onClick={ this.playClicked } />
+                                    : <Pause onClick={ this.pauseClicked } />
+                        }
                         <SkipNext onClick={this.forward} />
                         <label className={`form-check-label d-flex align-items-center ${cycling ? "control-toggled" : ""}`}>
                             <input onChange={this.didToggleCycle} type="checkbox" className="form-check-input d-none"
@@ -101,7 +145,7 @@ export default class Player extends Component<{}, PlayerState> {
                     <div className="d-flex justify-content-center align-items-center text-center">
                         <span id="track-current-time"></span>
                         <span id="seek-obj-container">
-                            <progress id="progress-bar" className="d-none d-md-inline-block" value="0"
+                            <progress id="progress-bar" className="d-none d-md-inline-block" value={ player ? player.currentTime / player.duration : 0 }
                                 max="1"></progress>
                         </span>
                         <small style={{float: "left", position: "relative", left: "15px"}} id="start-time"></small>
