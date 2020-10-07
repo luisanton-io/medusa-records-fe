f606a49ae59c0b9a19a69a9f3c8b5771c2f9168f
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-07 16:30:13 +0200

    Re-rendering app when refreshing tokens successfully.

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,10 @@ import fetchIntercept from 'fetch-intercept';
 import { Routes } from './routes/Routes';
 import { API } from './routes/API';
 
-let latestRequest: {url: string, config: any} = {url: '', config: {}}
-
 // const unregister = 
 fetchIntercept.register({
     request: function (url, config) {
         // Modify the url or config here
-        latestRequest = { url, config }
         return [url, config];
     },
 
@@ -24,34 +21,40 @@ fetchIntercept.register({
     },
 
     response: function (response) {
-        if (response.status === 403) {
+
+      switch (response.status) {
+        case 403:
+          console.log(response);
           (async () => await API.refreshToken())()
-        }
-
-        if (response.status === 222) {
-          (async () => await fetch(latestRequest.url, latestRequest.config))()
-        }
-
-        if (response.status === 401) {
+          break 
+        case 222: 
+          render() //Re-rendering after successfully getting a new refreshed token.
+          break
+        case 401:         
           window.location.pathname = Routes.public.login
-        }
+          break
+        default: break
+      }
+
         return response;
     },
 
     responseError: function (error) {
         // Handle an fetch error
-        console.log(error)
-        console.log("ciao")
         return Promise.reject(error);
     }
 });
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(
+    // <React.StrictMode>
+      <App />,
+    // </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
