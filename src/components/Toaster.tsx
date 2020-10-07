3de17d9537f38b2be69d6abd7361c558e28f9a62
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-07 18:38:29 +0200

    Update Toaster.tsx

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -6,30 +6,26 @@ import { Snackbar } from '@material-ui/core';
 import ToastComponent from '../models/Toast/component';
 
 export default class Toaster extends ToastComponent {
-
+    
     toast = this.hardBind(ToastComponent.shared.toast)
-
+    
     closeToast = (event?: React.SyntheticEvent, reason?: string) => {
         if (reason !== "clickaway") {
-            this.toast.value = ({
-                ...this.toast.value,
-                display: false
-            });
+            this.toast.value = ({ ...this.toast.value, display: false });
         }
     };
-
+    
     render() {
         const { display, severity, message } = this.toast.value
-        
         return (
             <Snackbar open={display}
-                autoHideDuration={6000} onClose={this.closeToast}>
+            autoHideDuration={6000} onClose={this.closeToast}>
                 <MuiAlert
                     elevation={6}
                     variant="filled"
                     onClose={this.closeToast}
                     severity={severity}
-                >
+                    >
                     { message }
                 </MuiAlert>
             </Snackbar>
