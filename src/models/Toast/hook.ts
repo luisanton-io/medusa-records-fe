8ff9475e88593b402cbc7b869e2658af926cafb5
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-07 18:37:53 +0200

    Now using React hook

--- src/models/Toast/hook.ts
+++ src/models/Toast/hook.ts
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import { Toast } from './interface'
 import ToastComponent from './component'
 
-export function useToast(_toast?: Toast) {
-    const toast = _toast || ToastComponent.shared.toast.value
-
-    const setToast = (t: Toast) => {
-        ToastComponent.shared.toast.value = t
-        ToastComponent.dispatchToChildren()
+export function useToast() {   
+    const [_toast, _setToast] = useState(ToastComponent.shared.toast.value) 
+    return { 
+        getToast: () => _toast, 
+        setToast: (newToast: Toast) => {
+            ToastComponent.shared.toast.value = newToast
+            ToastComponent.dispatchToChildren()
+            _setToast(newToast)
+        }
     }
-
-    return { toast, setToast }
 }
