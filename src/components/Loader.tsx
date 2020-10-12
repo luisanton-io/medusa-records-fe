574df7ed380c6da8ed58e2a1fd158c3eaef8ceb3
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-12 21:26:51 +0200

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CircularProgress } from '@material-ui/core'
 import { theme } from '../App'
 export default function Loader() {
-    return <div className="position-absolute bg-dark d-flex" style={{top: "0", left: "0", height: "100vh", width: "100vw"}}>
+    return <div className="position-absolute d-flex" style={{top: "0", left: "0", height: "100vh", width: "100vw", backgroundColor: `rgba(29,29,29,0.5)`}}>
         <div className="m-auto d-flex flex-column align-items-center" >
             <img className="img-fluid w-50 mb-5" src="/assets/medusa-original.png" alt="..." />
             <CircularProgress style={{ color: theme.palette.primary.dark }} />
