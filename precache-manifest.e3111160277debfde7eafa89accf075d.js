bcabc4a2df3ccafed570088c211a9bc445665a27
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-13 18:19:32 +0200

    Updates

---
+++ precache-manifest.e3111160277debfde7eafa89accf075d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecf1696ed1b230d54c975be89649e64",
+    "url": "/medusa-records-fe/index.html"
+  },
+  {
+    "revision": "c60957b00af014ffbf4d",
+    "url": "/medusa-records-fe/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "74dd005c53c2fb537089",
+    "url": "/medusa-records-fe/static/css/main.c0e499d7.chunk.css"
+  },
+  {
+    "revision": "c60957b00af014ffbf4d",
+    "url": "/medusa-records-fe/static/js/2.7359d98b.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/medusa-records-fe/static/js/2.7359d98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74dd005c53c2fb537089",
+    "url": "/medusa-records-fe/static/js/main.70d5e833.chunk.js"
+  },
+  {
+    "revision": "df6b55815a0cd3984ee0",
+    "url": "/medusa-records-fe/static/js/runtime-main.81608246.js"
+  }
+]);
