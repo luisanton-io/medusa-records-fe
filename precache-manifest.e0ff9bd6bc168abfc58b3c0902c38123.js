1a399e88e2f4df67ccb260ec64fb57c42e1320c6
luisanton-io <62301578+luisanton-io@users.noreply.github.com>
2020-10-13 22:51:32 +0200

    Updates

---
+++ precache-manifest.e0ff9bd6bc168abfc58b3c0902c38123.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc7862b080be04baf94f42a30ca586d",
+    "url": "/medusa-records-fe/index.html"
+  },
+  {
+    "revision": "b02e258a00ce54c10eca",
+    "url": "/medusa-records-fe/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1d8c9a4f0bc9a579efa4",
+    "url": "/medusa-records-fe/static/css/main.c0e499d7.chunk.css"
+  },
+  {
+    "revision": "b02e258a00ce54c10eca",
+    "url": "/medusa-records-fe/static/js/2.c89d614f.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/medusa-records-fe/static/js/2.c89d614f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8c9a4f0bc9a579efa4",
+    "url": "/medusa-records-fe/static/js/main.68683576.chunk.js"
+  },
+  {
+    "revision": "df6b55815a0cd3984ee0",
+    "url": "/medusa-records-fe/static/js/runtime-main.81608246.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60c08cacb48fff230cc6ea01188c64de.js"
+  "/medusa-records-fe/precache-manifest.e0ff9bd6bc168abfc58b3c0902c38123.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/medusa-records-fe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
